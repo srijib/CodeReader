11e9a026c88d49cadfb9aa2313960bc4902f8312
weikano <weikano@163.com>
2015-04-23 18:56:35 +0800

    fix

---
+++ days/src/main/java/com/wkswind/days/LockActivityFragment.java
@@ -0,0 +1,24 @@
+package com.wkswind.days;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class LockActivityFragment extends Fragment {
+
+    public LockActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lock, container, false);
+        return view;
+    }
+}
