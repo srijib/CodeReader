70eed5e573ad45f3d03b2283291908873b1c49e7
weikano <weikano@163.com>
2015-05-09 19:56:44 +0800

    hehe

--- money/src/main/java/com/wkswind/money/base/BaseFragment.java
+++ money/src/main/java/com/wkswind/money/base/BaseFragment.java
@@ -1,13 +1,22 @@
 package com.wkswind.money.base;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import com.squareup.leakcanary.RefWatcher;
+
+import net.simonvt.schematic.annotation.OnCreate;
 
 /**
  * Created by Administrator on 2015/5/9.
  */
 public class BaseFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
