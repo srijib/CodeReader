b495d064726dc0ca8eb2c7ffd1141ed0c48edcc8
weikano <weikano@163.com>
2015-06-08 19:30:53 +0800

    modify project's gradle file for comman libraries

---
+++ password/src/main/java/com/wkswind/password/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.wkswind.password.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2015/6/5.
+ */
+public class BaseActivity extends AppCompatActivity{
+}
---
+++ password/src/main/java/com/wkswind/password/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.wkswind.password.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2015/6/5.
+ */
+public class BaseFragment extends Fragment {
+
+}
