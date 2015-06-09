238028a02c8d372897bf82f07a317f07c5655a80
weikano <weikano@163.com>
2015-06-09 19:02:54 +0800

    databases

---
+++ password/src/main/java/com/wkswind/password/LockActivity.java
@@ -0,0 +1,18 @@
+package com.wkswind.password;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Toast;
+
+import com.wkswind.password.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2015/6/9.
+ */
+public class LockActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock);
+    }
+}
---
+++ password/src/main/java/com/wkswind/password/databases/Password.java
@@ -0,0 +1,48 @@
+package com.wkswind.password.databases;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.ForeignKey;
+import com.raizlabs.android.dbflow.annotation.ForeignKeyReference;
+import com.raizlabs.android.dbflow.annotation.OneToMany;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.sql.builder.Condition;
+import com.raizlabs.android.dbflow.sql.language.Select;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+import com.raizlabs.android.dbflow.structure.container.ForeignKeyContainer;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/6/9.
+ */
+@Table(databaseName = AppDatabase.NAME)
+public class Password extends BaseModel {
+    @Column
+    @PrimaryKey(autoincrement = true)
+    long id;
+    @Column
+    String name;
+    @Column
+    @ForeignKey(references = {@ForeignKeyReference(columnName = "type_id", columnType = Long.class, foreignColumnName = "id")}, saveForeignKeyModel = false)
+    PasswordType passwordType;
+    @Column
+    @ForeignKey(
+            references = {@ForeignKeyReference(columnName = "question_id",
+                    columnType = Long.class,
+                    foreignColumnName = "id")},
+            saveForeignKeyModel = false)
+    ForeignKeyContainer<SafetyQuestion> safetyQuestions;
+    @Column
+    int status;
+    List<SafetyQuestion> allSafetyQuestions;
+
+    @OneToMany(methods = {OneToMany.Method.ALL})
+    public List<SafetyQuestion> getAllSafetyQuestions(){
+        if(allSafetyQuestions == null){
+            allSafetyQuestions = new Select().from(SafetyQuestion.class).where(Condition.column(SafetyQuestion$Table.PASSWORD_PASSWORD_ID).is(id)).queryList();
+        }
+        return allSafetyQuestions;
+    }
+
+}
