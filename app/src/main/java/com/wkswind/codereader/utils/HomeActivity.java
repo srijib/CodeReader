7367e593003de2192df0ededac3fa3ce202a7f10
南风不竞 <weikano@163.com>
2015-11-16 20:58:11 +0800

    hehe

---
+++ app/src/main/java/com/wkswind/codereader/utils/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.wkswind.codereader.utils;
+
+import android.os.Bundle;
+
+import com.wkswind.codereader.BaseActivity;
+
+/**
+ * Created by Administrator on 2015-11-16.
+ */
+public class HomeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
